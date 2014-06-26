62cd616c9a9fa9a86628a754bd6d8be19ff0d1a4
Norbert Melzer <timmelzer@gmail.com>
2014-06-26 09:33:17 +0200

    restructure PaperFormat

--- src/Pager/PaperFormat.hs
+++ src/Pager/PaperFormat.hs
@@ -26,6 +26,8 @@ module Pager.PaperFormat ( Points     (Points)
                          , minch2mm
                          , toPDFRect) where
 
+import           Data.List
+import           Data.Maybe
 import           Data.Tuple
 import qualified Graphics.PDF as PDF
 
@@ -123,6 +125,7 @@ data PaperFormat = A0 | A1 | A2 | A3 | A4 | A5 | A6 | A7 | A8
                  | D0
                  | SRA0 | SRA1 | SRA2 | SRA3 | SRA4
                  |  RA0 |  RA1 |  RA2
+                 deriving (Eq, Show)
 
 -- | Describes the orientation of the paper
 data Orientation = Landscape -- ^ the paper is oriented in a way that the width is greater than the height
@@ -134,17 +137,35 @@ toPDFRect (PaperSize   o pf) = case o of
   Landscape -> uncurry (PDF.PDFRect 0 0) (swap $ dimension)
   Portrait  -> uncurry (PDF.PDFRect 0 0) (       dimension)
   where
-    dimension = case pf of
-      A0 -> (2384, 3370)
-      A1 -> (1684, 2384)
-      A2 -> (1190, 1684)
-      A3 -> ( 842, 1190)
-      A4 -> ( 595,  842)
-      A5 -> ( 420,  595)
-      A6 -> ( 298,  420)
-      A7 -> ( 210,  298)
-      A8 -> ( 148,  210)
-      _  -> (   0,    0)
+    dimension = maybe (0, 0) id $ lookup pf dimensionList
 toPDFRect (PaperRectPt w h ) = PDF.PDFRect 0 0 (ub              w) (ub              h)
 toPDFRect (PaperRectMM w h ) = PDF.PDFRect 0 0 (ub . mm2pt    $ w) (ub . mm2pt    $ h)
 toPDFRect (PaperRectMI w h ) = PDF.PDFRect 0 0 (ub . minch2pt $ w) (ub . minch2pt $ h)
+
+dimensionList :: [(PaperFormat, (Int, Int))]
+dimensionList = [ (A0, (2384, 3370))
+                , (A1, (1684, 2384))
+                , (A2, (1190, 1684))
+                , (A3, ( 842, 1190))
+                , (A4, ( 595,  842))
+                , (A5, ( 420,  595))
+                , (A6, ( 298,  420))
+                , (A7, ( 210,  298))
+                , (A8, ( 148,  210))
+                , (B0, (2835, 4008))
+                , (B1, (2004, 2835))
+                , (B2, (1417, 2004))
+                , (B3, (1001, 1417))
+                , (B4, ( 709, 1001))
+                , (B5, ( 499,  709))
+                , (B6, ( 354,  499))
+                , (B7, ( 249,  354))
+                , (B8, ( 176,  249))
+                , (B9, ( 125,  176))
+                , (B10,(  88,  125))
+                , (C2, (1837,  578))
+                , (C3, (0,0))
+                , (C4, (0,0))
+
+                ]
+
