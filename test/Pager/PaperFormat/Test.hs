acccfca888867ce55ffc799a9d189c05a5e14306
Norbert Melzer <timmelzer@gmail.com>
2014-06-27 06:52:48 +0200

    Cleaned PaperFormats

--- test/Pager/PaperFormat/Test.hs
+++ test/Pager/PaperFormat/Test.hs
@@ -9,7 +9,7 @@ import           Test.SmallCheck.Series
 import           Test.Tasty
 import           Test.Tasty.SmallCheck
 
-import           Graphics.PDF
+import qualified Graphics.PDF           as PDF
 
 import           Pager.PaperFormat
 
@@ -17,9 +17,7 @@ formats :: a -> [PaperFormat]
 formats _ = [ A0, A1, A2, A3, A4, A5, A6, A7, A8
             , B0, B1, B2, B3, B4, B5, B6, B7, B8, B9, B10
             ,         C2, C3, C4, C5, C6
-            , D0
-            , SRA0, SRA1, SRA2, SRA3, SRA4
-            ,  RA0,  RA1,  RA2
+            , Letter, Legal
             ]
 
 instance Monad m => Serial m PaperFormat where
@@ -32,5 +30,5 @@ paperFormatSuite = testGroup "PaperFormat"
 
 size_prop :: Property IO
 size_prop = forAll $ \pf -> case toPDFRect (PaperSize Landscape pf) of
-  PDFRect 0 0 w h -> (w, h) > (0, 0)
-  PDFRect _ _ _ _ -> False
+  PDF.PDFRect 0 0 w h -> (w, h) > (0, 0)
+  PDF.PDFRect _ _ _ _ -> False
